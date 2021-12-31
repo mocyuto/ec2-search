0e4b876669bb8d9753feb279d6e30c15f4513aef
Yuto Suzuki <yuutoo.advance@gmail.com>
2021-12-31 17:26:18 +0900

    set extern crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+extern crate aws_sdk_autoscaling;
+extern crate aws_sdk_ec2;
 extern crate regex;
 extern crate roxmltree;
 extern crate rusoto_core;
 extern crate rusoto_elbv2;
+
 #[macro_use]
 extern crate lazy_static;
 

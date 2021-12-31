ef3c1225964884ee2b5796f347a292ab815cdd0c
Yuto Suzuki <yuutoo.advance@gmail.com>
2021-12-31 16:14:10 +0900

    change ec2 sdk to official

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 extern crate regex;
 extern crate roxmltree;
-extern crate rusoto_autoscaling;
 extern crate rusoto_core;
-extern crate rusoto_ec2;
 extern crate rusoto_elbv2;
 #[macro_use]
 extern crate lazy_static;

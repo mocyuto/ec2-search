27101797873ce7f056b4edb9dba7691d282326a1
Yuto Suzuki <yuutoo.advance@gmail.com>
2022-03-06 23:22:22 +0900

    rm unused extern crate, and change lazy_static to once_lazy

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-extern crate aws_sdk_autoscaling;
-extern crate aws_sdk_ec2;
-extern crate aws_sdk_elasticloadbalancingv2;
-extern crate regex;
-
-#[macro_use]
-extern crate lazy_static;
-
 pub mod autoscaling;
 pub mod awsutils;
 pub mod instance;
